dfac34f1b545007315aaac9fe5c1685cf275381b
Juan Montoya <juanmontoya015@gmail.com>
2023-12-03 13:06:44 -0500

    [NEW]: Nuevo repositorio Gestor-Eventos-SQS
    Inicio de segunda actividad para el proyecto de Gestor de Eventos SQS

---
+++ src/main/java/edu/jumogira/gestoreventossqs/GestorEventosSqsApplication.java
@@ -0,0 +1,13 @@
+package edu.jumogira.gestoreventossqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorEventosSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorEventosSqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/jumogira/gestoreventossqs/GestorEventosSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jumogira.gestoreventossqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorEventosSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
